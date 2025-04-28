32535145351aacc612413ed034f747e1ae25b134
artha <U21101466@sharjah.ac.ae>
2025-04-29 02:19:11 +0400

    Delete startup.sh

--- startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Make the script executable
-chmod +x startup.sh
-
-# Start the application on port 8000 to work with Azure App Service
-gunicorn --bind 0.0.0.0:8000 --reuse-port --timeout 600 main:app
